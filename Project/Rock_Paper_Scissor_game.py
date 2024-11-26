8e3bf1563a9753616fc009d60ac434bde2209605
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-11-26 13:05:30 +0500

    changes

---
+++ Project/Rock_Paper_Scissor_game.py
@@ -0,0 +1,24 @@
+"""
+    Rock Paper Scissor Game
+    
+    In this game, you can
+    
+    1. play against the computer
+    computer will choose a random (rock, paper, scissor)
+    
+Concept of Python Used here:
+
+1)Manipulation of list
+2)Condition
+
+
+
+WorkFLow of the project:
+
+1) Input From user (Rock,Paper, Scissor)
+2) Computer will choose a random (rock, paper, scissor)
+3) Print result (Win, Lose, Draw)
+
+
+    
+"""

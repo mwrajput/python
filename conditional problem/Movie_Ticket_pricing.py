b1e70a3cf1b66fe2a8f51ea331059553d7389c63
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-11-27 15:20:18 +0500

    new prob solve

--- conditional problem/Movie_Ticket_pricing.py
+++ conditional problem/Movie_Ticket_pricing.py
@@ -11,16 +11,28 @@ WorkFlow Details:
 
 """
 
+
+# age = int(input('Enter your Age : '))
+# day = str(input('Enter What day is today : '))
+
+# if day == "wednesday":
+#         print('Ticket Price is $2')
+# else:
+#     if age < 18 :
+#         print('Ticket Price is 8$')
+#     elif age >= 18 :
+#         print('Ticket Price is 12$')
+#     else:
+#         print('Invalid')    
+        
 age = int(input('Enter your Age : '))
 day = str(input('Enter What day is today : '))
 
+price = 12  if age >= 18 else  8
+
 if day == "wednesday":
-        print('Ticket Price is $2')
+    price = price - 2
+    print(f'Ticket Price is : {price}$. Enjoy Discount!')
 else:
-    if age < 18 :
-        print('Ticket Price is 8$')
-    elif age >= 18 :
-        print('Ticket Price is 12$')
-    else:
-        print('Invalid')    
-        
+    print(f'Ticket Price is : {price}$')
+    

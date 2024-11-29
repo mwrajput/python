2d76f23ea15866e2f93df7d1c86afd699574398e
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-11-29 14:33:11 +0500

    question

--- Loops problem in python/findNonRepeatedCHar.py
+++ Loops problem in python/findNonRepeatedCHar.py
@@ -1,7 +1,19 @@
 """
 Find the First Non-Repeated Character
 
+its just give non repeated character
+
 """
 
-str = "Hello kyse hn ap"
+
+# MY_str = "Hello kyse hn ap k k"
+# MY_str = "Muhammad Waqas "
+MY_str = "Python programming is fun!"
+
+for char in MY_str:
+    # print(char ," : " ,MY_str.count(char))
+    if MY_str.count(char) == 1 :
+        print("Char : " , char)
+        # as we want only frist character that is not repeated
+        break;
 

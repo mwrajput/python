0faa5140549e195147deff0cb315f77393aee971
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-12-19 09:00:03 +0500

    a

---
+++ Sets/part1.py
@@ -0,0 +1,23 @@
+my_var = {1,2,3,4,5}
+
+# print(my_var)
+
+# set() function or set method
+# another_Set = set([10,30,40,50])
+# another_Set = {}
+# print(another_Set)
+# print(type(another_Set)) 
+
+# Properties of Sets
+
+# unordered
+# setA = {"animal" , "ball" , 54 , True , False }
+# print(setA)
+
+menu = ['stewed meat with onions', 'bean soup', 'risotto with trout and shrimp',
+       'fish soup with cream and onion', 'gyro']
+
+# TODO: remove 'bean soup', and add 'roasted beet salad' to the end of the menu
+menu.remove("bean soup")
+menu.append('roasted beet salad')
+print(menu)

f876023275ac8d82a66da14e0fe24a59684bb054
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-12-22 12:30:42 +0500

    update

--- OOPs/isINstanceFun.py
+++ OOPs/isINstanceFun.py
@@ -8,20 +8,16 @@ class Car:
     def __init__(self, brand, model):
         # making brand private
        self.__brand = brand
-       self.__model = model
+       self.model = model
        
     def get_brand(self):
         return self.__brand + " : Your Car brand "
     def full_name(self):
         return f"{self.__brand} {self.model}"
 
-    @staticmethod
     def general_descriptions():
         return "Cars are  means of transport"
     
-    @property
-    def model(self):
-        return self.__model
 class ElectricCar(Car):
     def __init__(self , brand ,model ,battery_size):
         
@@ -30,13 +26,11 @@ class ElectricCar(Car):
         super().full_name()
         
 my_Tesla = ElectricCar("Tesla" , "model_101_S" ,"85kWH" )
-# print(my_Tesla.model)
-# print(my_Tesla.full_name())
-# print(my_Tesla.get_brand())
+print(my_Tesla.model)
+print(my_Tesla.full_name())
+print(my_Tesla.get_brand())
 
-my_car = Car("Honda" , "Civic")
-my_car.model = "City"
-print(my_car.model())
-# print(my_car.general_descriptions())
-print(Car.general_descriptions())
+print(isinstance(my_Tesla ,Car))
+print(isinstance(my_Tesla, ElectricCar))
+
         
--- TheZenofPython.py
+++ TheZenofPython.py
@@ -21,11 +21,18 @@
 # If the implementation is hard to explain, it's a bad idea.
 # If the implementation is easy to explain, it may be a good idea.   
 # Namespaces are one honking great idea -- let's do more of those! 
-
-class Dog:
-
-  species = "Canis familiaris"
-
-  def __init__(self, name, age):
-      self.name = name
-      self.age = age
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+        
+        
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")

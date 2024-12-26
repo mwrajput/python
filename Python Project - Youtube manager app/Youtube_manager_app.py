5342b653322206e5297c1f264c8f32ceb371e659
Muhammad Waqas <125763001+mwrajput@users.noreply.github.com>
2024-12-26 23:14:42 +0500

    updated

--- Python Project - Youtube manager app/Youtube_manager_app.py
+++ Python Project - Youtube manager app/Youtube_manager_app.py
@@ -15,8 +15,12 @@ def save_data_helper(videos):
         json.dump(videos, file)
 
 def list_all_videos(video):
+    print("\n\nItems in the list are :")
+    print("*"*70)
     for index, video in enumerate(video, start=1):
-        print(f"{index}.")
+        print(f"{index}) Name :{video['name']}, Duration:{video['time']} ")  
+    
+    print("*"*70)
 
 def add_video(videos):
     name = input("Enter Video Name : ")
@@ -25,25 +29,38 @@ def add_video(videos):
     save_data_helper(videos)
     
 def update_video(videos):
-    pass
-
+    list_all_videos(videos)
+    index = int(input('Enter the video number to update : '))
+    if 1 <= index <= len(videos):
+       name =input('Enter the new video name : ')
+       time = input('Enter the new video time ')
+       videos[index-1] = {'name':name, 'time':time}
+       save_data_helper(videos)
+    else:
+        print("Invalid Text selected")
+        
 def remove_video(videos):
-    pass
-
+    list_all_videos(videos)
+    index = int(input('Enter the video number to be deleted : '))
+    if 1<=index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid Index selected")
 def main():
     videos = load_data()
 
     while True:
-        print("\n Youtube Manager | Choose an Option ")
-        print("\n 1. List a Fav youtube Video ")
-        print("\n 2. Add a Youtube Video ")
-        print("\n 3. Update a youtube Video ")
-        print("\n 4. Delete a Youtube Video ")
-        print("\n 5.Exit the App ")
+        print("\n Youtube Manager | Choose an Option\n")
+        print(" 1. List a Fav youtube Video ")
+        print(" 2. Add a Youtube Video ")
+        print(" 3. Update a youtube Video ")
+        print(" 4. Delete a Youtube Video ")
+        print(" 5. Exit the App \n")
         
         # input for user choice
         Choice = input("Enter Your Choice : ")
-        print(videos)
+        # print(videos)
         
         match Choice:
             case '1':
---
+++ project with sqlite3/Youtube_manager_with_sqlite3.py
@@ -0,0 +1,70 @@
+import sqlite3
+conn = sqlite3.connect('youtube_videos.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+        CREATE TABLE IF NOT EXISTS videos(
+            id INTEGER PRIMARY KEY ,
+            name TEXT NOT NULL,
+            time Text NOT NULL
+        )               
+''')
+
+def list_videos():
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+        print(row)
+
+def add_videos(name,time):
+    cursor.execute("INSERT INTO videos (name,time) VALUES (?, ?)",(name,time))
+    conn.commit()
+
+def update_videos(vid_id,new_name,new_time):
+    list_videos()
+    cursor.execute("UPDATE videos SET name = ? ,time = ? where id = ?",(new_name, new_time,vid_id))
+    conn.commit()
+
+def delete_videos(vid_id):
+    list_videos()
+    cursor.execute("DELETE FROM videos WHERE id = ?",(vid_id,))
+    conn.commit()
+
+
+def main():
+        while True:
+            print("\n Youtube Manager | Choose an Option\n")
+            print(" 1. List a Fav youtube Video")
+            print(" 2. Add a Youtube Video" )
+            print(" 3. Update a youtube Video")
+            print(" 4. Delete a Youtube Video ")
+            print(" 5. Exit the App \n")
+            
+            # input for user choice
+            choice = input("Enter Your Choice : ")
+            # print(videos)
+            
+            if choice == '1':
+                list_videos()
+            elif choice == '2':
+                name = input('Enter your video name : ')
+                time = input('Enter your video time : ')
+                add_videos(name,time)
+            elif choice == '3':
+                vid_id = input('Enter your video ID : ')
+                name = input('Enter your video  name : ')
+                time = input('Enter your video time : ')
+                update_videos(vid_id,name,time)
+            elif choice == '4':
+                vid_id = input('Enter your video ID to delete : ')
+                delete_videos(vid_id)
+            elif choice == '5':
+                break
+            else:
+                print("Invalid Choice")
+    
+        conn.close()   
+        
+        
+                 
+if __name__ == '__main__':
+    main()
